2d6fa6b2b6da4c8caa6d343bca56654b21070625
TubaDemir <tubademir04@hotmail.com>
2019-03-06 01:32:40 +0300

    N-Tier Architecture

---
+++ NorthWind-ORM-UI/NorthWind.ORM/Tools.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace NorthWind.ORM
+{
+    public class Tools
+    {
+        // UI projesinde App.config icerisine baglanti parametresi eklenir.
+        internal static SqlConnection baglanti = new SqlConnection(ConfigurationManager.ConnectionStrings["Baglanti"].ConnectionString);
+        
+        internal static bool ExecuteNonQuery(SqlCommand cmd)
+        {
+            try
+            {
+                if (cmd.Connection.State != ConnectionState.Open)
+                    cmd.Connection.Open();
+                int affected = cmd.ExecuteNonQuery();
+                if (cmd.Connection.State != ConnectionState.Closed)
+                    cmd.Connection.Close();
+                return (affected > 0);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                if (cmd.Connection.State != ConnectionState.Closed)
+                    cmd.Connection.Close();
+            }
+        }
+    }
+}
